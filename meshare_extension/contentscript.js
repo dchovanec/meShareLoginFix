73471bd022bea30516c4c309d8cad1df063298e3
dchovanec <david@chovanec.name>
2016-02-26 20:57:25 +0100

    fixed invalid comment

--- meshare_extension/contentscript.js
+++ meshare_extension/contentscript.js
@@ -1,6 +1,6 @@
-# based on this post:
-# http://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
-#
+// based on this post:
+// http://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
+//
 
 var s = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
